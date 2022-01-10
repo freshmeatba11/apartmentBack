4e78bcf2c498907060fda868c8dd45b33a0fb06d
meatba11 <cmjj8898212032@gmail.com>
2022-01-10 18:05:15 +0800

    add auth-route & register-joi-validation

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const registerValidation = require("../validation").registerValidation;
+const loginValidation = require("../validation").loginValidation;
+
+router.use((req, res, next) => {
+  console.log("A request is coming in to auth.js .");
+  next();
+});
+
+router.get("/testAPI", (req, res) => {
+  const msgObj = {
+    message: "Test API is working.",
+  };
+  return res.json(msgObj);
+});
+
+router.post("/register", (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+});
+
+module.exports = router;
